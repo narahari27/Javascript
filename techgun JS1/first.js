7bbd2c4caab8ae85a9853028d4ae28b4014e53c3
narhari27 <narahari.eng@gmail.com>
2023-09-27 20:44:42 +0530

    changes

---
+++ techgun JS1/first.js
@@ -0,0 +1,4 @@
+// document.getElementById("h").innerHTML="Narahari"
+// document.write("here you go")
+// console.log("5+6");
+alert("5+6");
---
+++ techgun JS1/script.js
@@ -0,0 +1,10 @@
+// let digit1;
+// //const x =2;
+
+// //let z = digit1 + x;
+// digit1="narahari";
+
+let a = "hey my name";
+let b = "is narahari";
+
+console.log(a+ " " +b);
